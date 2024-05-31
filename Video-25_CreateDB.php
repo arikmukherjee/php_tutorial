0143e3e8155c67acfdf9541a393f93018bd24813
Arik Mukherjee <112742649+arikmukherjee@users.noreply.github.com>
2024-05-31 20:01:04 +0530

    Add files via upload

---
+++ Video-25_CreateDB.php
@@ -0,0 +1,32 @@
+<?php
+// SOURCE: https://www.codewithharry.com/videos/php-tutorials-in-hindi-25/
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful<br>";
+}
+
+// Create a db
+$sql = "CREATE DATABASE 2022_Student_DB";
+$result = mysqli_query($conn, $sql); //executes the database.
+// echo var_dump($result);
+
+// Check for the database creation success
+if($result){
+    echo "The db was created successfully!<br>";
+}
+else{
+    echo "The db was not created successfully because of this error ---> ". mysqli_error($conn);
+}
+  
+?>
---
+++ Video-27_Insert_Data.php
@@ -0,0 +1,36 @@
+<!-- SOURCE: https://www.codewithharry.com/videos/php-tutorials-in-hindi-27/ -->
+
+<?php
+
+// Connecting to the Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_arik";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+else{
+    echo "Connection was successful<br>";
+}
+
+// Variables to be inserted into the table
+$name = "Vikrant";
+$destination = "Russia";
+
+// Sql query to be executed
+$sql = "INSERT INTO `phptrip` (`name`, `dest`) VALUES ('$name', '$destination')";
+$result = mysqli_query($conn, $sql);
+
+// Add a new trip to the Trip table in the database
+if($result){
+    echo "The record has been inserted successfully!<br>";
+}
+else{
+    echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+}
+?>
